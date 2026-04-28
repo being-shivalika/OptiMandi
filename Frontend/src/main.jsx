81253ea05ff7baf4c5618b3e35a23cfa80a3ee8f
being-shivalika <shivalika271@gmail.com>
2026-04-28 22:21:07 +0530

    tried my best

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
-import { DataProvider } from "./context/DataProvider.jsx";
+import DataProvider from "../context/DataProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
